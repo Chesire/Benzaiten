cb02aca6ee286a819d70ecbbd9e60d0e0a2f18aa
Chesire <troyrijkaard@hotmail.co.uk>
2022-05-01 15:49:06 +0100

    feat: update StatusPage to log error
    When an error occurs and the StatusPage catches it, log it out.

--- src/main/kotlin/com/chesire/benzaiten/plugins/StatusPages.kt
+++ src/main/kotlin/com/chesire/benzaiten/plugins/StatusPages.kt
@@ -3,6 +3,7 @@ package com.chesire.benzaiten.plugins
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.application.log
 import io.ktor.server.plugins.StatusPages
 import io.ktor.server.response.respond
 
@@ -12,6 +13,7 @@ import io.ktor.server.response.respond
 fun Application.configureStatusPages() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
+            log.error("Global error - $cause")
             call.respond(HttpStatusCode.InternalServerError)
         }
     }
