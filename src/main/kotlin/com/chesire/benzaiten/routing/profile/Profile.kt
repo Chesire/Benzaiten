44d1cf909273883dfb18bab409ffa3c595ad4d8e
Chesire <troyrijkaard@hotmail.co.uk>
2022-05-01 15:54:49 +0100

    feat: call the Spotify profile endpoint
    Make a call to the Spotify profile endpoint, and return the payload back.

---
+++ src/main/kotlin/com/chesire/benzaiten/routing/profile/Profile.kt
@@ -0,0 +1,47 @@
+package com.chesire.benzaiten.routing.profile
+
+import com.chesire.benzaiten.Const.SPOTIFY_TOKEN
+import com.chesire.benzaiten.plugins.koin.inject
+import com.chesire.benzaiten.routing.ErrorDomain
+import com.chesire.benzaiten.routing.profile.service.ProfileService
+import com.github.michaelbull.result.onFailure
+import com.github.michaelbull.result.onSuccess
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+
+/**
+ * Route to access details about a users profile.
+ */
+fun Route.profile() {
+    val service by inject<ProfileService>()
+
+    /**
+     * Must have the spotify token in the headers.
+     */
+    get("profile/") {
+        val token = call.request.headers[SPOTIFY_TOKEN]
+        if (token == null) {
+            call.respond(
+                ErrorDomain(
+                    "No spotify token",
+                    "Please provide a spotify token in the headers."
+                )
+            )
+            return@get
+        }
+
+        service.retrieveUserProfile(token)
+            .onSuccess { call.respondText { it.toString() } }
+            .onFailure {
+                call.respond(
+                    ErrorDomain(
+                        it.status.toString(),
+                        it.message
+                    )
+                )
+            }
+    }
+}
---
+++ src/main/kotlin/com/chesire/benzaiten/routing/profile/data/SpotifyProfileDomain.kt
@@ -0,0 +1,63 @@
+package com.chesire.benzaiten.routing.profile.data
+
+import kotlinx.serialization.SerialName
+
+@kotlinx.serialization.Serializable
+data class SpotifyProfileDto(
+    @SerialName("country")
+    val country: String,
+    @SerialName("display_name")
+    val displayName: String,
+    @SerialName("email")
+    val email: String,
+    @SerialName("explicit_content")
+    val explicitContent: ExplicitContent,
+    @SerialName("external_urls")
+    val externalUrls: ExternalUrls,
+    @SerialName("followers")
+    val followers: Followers,
+    @SerialName("href")
+    val href: String,
+    @SerialName("id")
+    val id: String,
+    @SerialName("images")
+    val images: List<Image>,
+    @SerialName("product")
+    val product: String,
+    @SerialName("type")
+    val type: String,
+    @SerialName("uri")
+    val uri: String
+)
+
+@kotlinx.serialization.Serializable
+data class ExplicitContent(
+    @SerialName("filter_enabled")
+    val filterEnabled: Boolean,
+    @SerialName("filter_locked")
+    val filterLocked: Boolean
+)
+
+@kotlinx.serialization.Serializable
+data class ExternalUrls(
+    @SerialName("spotify")
+    val spotify: String
+)
+
+@kotlinx.serialization.Serializable
+data class Followers(
+    @SerialName("href")
+    val href: String?,
+    @SerialName("total")
+    val total: Int
+)
+
+@kotlinx.serialization.Serializable
+data class Image(
+    @SerialName("height")
+    val height: Int?,
+    @SerialName("url")
+    val url: String,
+    @SerialName("width")
+    val width: Int?
+)
