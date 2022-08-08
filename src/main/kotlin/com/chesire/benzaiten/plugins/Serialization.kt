f432e086dde3b41bbf9baae64a3045ee153ea3e9
Chesire <troy.rijkaard@chesire.dev>
2022-08-08 18:14:59 +0100

    chore: update ktor and koin dependencies

--- src/main/kotlin/com/chesire/benzaiten/plugins/Serialization.kt
+++ src/main/kotlin/com/chesire/benzaiten/plugins/Serialization.kt
@@ -3,7 +3,7 @@ package com.chesire.benzaiten.plugins
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.plugins.ContentNegotiation
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
 /**
  * Configures the [ContentNegotiation] plugin.
--- src/main/kotlin/com/chesire/benzaiten/plugins/koin/Koin.kt
+++ src/main/kotlin/com/chesire/benzaiten/plugins/koin/Koin.kt
@@ -3,13 +3,13 @@ package com.chesire.benzaiten.plugins.koin
 import com.chesire.benzaiten.plugins.koin.modules.defaultModules
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import org.koin.ktor.ext.Koin
+import org.koin.ktor.plugin.Koin
 
 /**
  * Installs and configures the [Koin] plugin.
  */
 fun Application.configureKoin() {
-    install(KoinPlugin) {
+    install(Koin) {
         modules(defaultModules)
     }
 }
