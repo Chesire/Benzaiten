754bfd170d702e30a6027b242fa85f93caf354ba
Chesire <troyrijkaard@hotmail.co.uk>
2022-08-20 09:32:45 +0100

    feat: add json annotations for other data classes

--- src/main/kotlin/com/chesire/benzaiten/service/playlist/SpotifyPlaylistDomain.kt
+++ src/main/kotlin/com/chesire/benzaiten/service/playlist/SpotifyPlaylistDomain.kt
@@ -24,34 +24,56 @@ data class SpotifyPlaylistDto(
 
 @kotlinx.serialization.Serializable
 data class SpotifyPlaylistDetails(
+    @SerialName("collaborative")
     val collaborative: Boolean,
+    @SerialName("description")
     val description: String,
+    @SerialName("externalUrls")
     val externalUrls: ExternalUrls, // TODO: Make this generic
+    @SerialName("href")
     val href: String,
+    @SerialName("id")
     val id: String,
+    @SerialName("images")
     val images: List<Image>, // TODO: Make this generic
+    @SerialName("name")
     val name: String,
+    @SerialName("owner")
     val owner: SpotifyPlaylistOwner,
+    @SerialName("primaryColor")
     val primaryColor: String?, // TODO: Not sure what this can be
+    @SerialName("public")
     val public: Boolean,
+    @SerialName("snapshotId")
     val snapshotId: String,
+    @SerialName("tracks")
     val tracks: SpotifyPlaylistTracks,
+    @SerialName("type")
     val type: String,
+    @SerialName("uri")
     val uri: String
 )
 
 @kotlinx.serialization.Serializable
 data class SpotifyPlaylistOwner(
+    @SerialName("displayName")
     val displayName: String,
+    @SerialName("externalUrls")
     val externalUrls: ExternalUrls,
+    @SerialName("href")
     val href: String,
+    @SerialName("id")
     val id: String,
+    @SerialName("type")
     val type: String,
+    @SerialName("uri")
     val uri: String
 )
 
 @kotlinx.serialization.Serializable
 data class SpotifyPlaylistTracks(
+    @SerialName("href")
     val href: String,
+    @SerialName("total")
     val total: Int
 )
